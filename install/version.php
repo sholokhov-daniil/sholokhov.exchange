25efe88d8b59aa349dc485b719670a78e4d3c5c3
Шолохов Даниил <sholokhovdaniil@yandex.ru>
2025-10-19 00:59:56 +0700

    work(doc): Создание документации

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 
 <?php
 $arModuleVersion = array(
-    "VERSION" => "2.1.0",
-    "VERSION_DATE" => "2025-10-18 14:21:00"
+    "VERSION" => "2.1.1",
+    "VERSION_DATE" => "2025-10-19 00:59:00"
 );
