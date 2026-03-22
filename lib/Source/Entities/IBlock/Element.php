73d5d34118cdf96202db3c36de5a45b0bf9e0d3e
Шолохов Даниил <sholokhovdaniil@yandex.ru>
2026-03-22 23:41:19 +0700

    work(test): Создание теста источника данных элементов инфоблока

--- lib/Source/Entities/IBlock/Element.php
+++ lib/Source/Entities/IBlock/Element.php
@@ -3,78 +3,309 @@
 namespace Sholokhov\Exchange\Source\Entities\IBlock;
 
 use Iterator;
-use ArrayIterator;
+use CIBlockResult;
 
-use Sholokhov\Exchange\Exception\Source\SourceException;
-use Sholokhov\Exchange\Source\IterableTrait;
 use Sholokhov\Exchange\Provider\Entity\IBlockElementProvider;
 use Sholokhov\Exchange\Provider\Entity\IBlockElementProviderInterface;
 
 /**
- * Источник данных основан на элементах информационного блока
+ * Источник данных основан на элементах информационного блока (IBlock)
  *
- * @author Daniil S.
+ * Поддерживает ленивую загрузку данных по батчам с фильтрацией, выборкой полей
+ * и свойств. Реализует интерфейс Iterator для удобной итерации через foreach.
+ *
+ * Особенности:
+ * - Поддержка батчевой загрузки через ID
+ * - Возможность выбирать поля и свойства элементов
+ * - Минимальное потребление памяти при больших объёмах данных
  *
  * @package Source
  */
 class Element implements Iterator
 {
-    use IterableTrait;
+    /**
+     * Размер пакета данных (батч)
+     *
+     * @var int
+     */
+    protected int $limit = 2000;
 
-    protected readonly array $options;
+    /**
+     * Последний обработанный ID (для батчевой загрузки)
+     *
+     * @var int
+     */
+    protected int $lastId = 0;
+
+    /**
+     * Фильтр для выборки элементов ИБ
+     *
+     * @var array
+     */
+    protected array $filter = [];
+
+    /**
+     * Поля для выборки
+     *
+     * @var array
+     */
+    protected array $select = ['ID', 'IBLOCK_ID', 'NAME'];
+
+    /**
+     * Свойства элементов, которые нужно загрузить
+     *
+     * @var array
+     */
+    protected array $properties = [];
+
+    /**
+     * Текущий батч элементов
+     *
+     * @var array
+     */
+    protected array $batch = [];
+
+    /**
+     * Признак окончания итерации
+     *
+     * @var bool
+     */
+    protected bool $finished = false;
+
+    /**
+     * Провайдер элементов ИБ
+     *
+     * @var IBlockElementProviderInterface
+     */
     protected readonly IBlockElementProviderInterface $provider;
 
-    public function __construct(array $options, ?IBlockElementProviderInterface $provider = null)
+    /**
+     * @param IBlockElementProviderInterface|null $provider Провайдер элементов ИБ
+     */
+    public function __construct(?IBlockElementProviderInterface $provider = null)
     {
-        $this->options = $options;
         $this->provider = $provider ?? new IBlockElementProvider;
+    }
 
-        if (empty($this->options['FILTER']) || !is_array($this->options['FILTER'])) {
-            throw new SourceException("Option 'FILTER' must be an array");
+    /**
+     * Устанавливает лимит элементов на один батч
+     *
+     * @param int $limit
+     *
+     * @return $this
+     */
+    public function setLimit(int $limit): static
+    {
+        $this->limit = $limit;
+        return $this;
+    }
+
+    /**
+     * Устанавливает ID информационного блока
+     *
+     * @param int $id ID информационного блока
+     *
+     * @return $this
+     */
+    public function setIBlockId(int $id): static
+    {
+        $this->filter['=IBLOCK_ID'] = $id;
+        return $this;
+    }
+
+    /**
+     * Устанавливает фильтр для выборки элементов
+     *
+     * @param array $filter Ассоциативный массив фильтра Bitrix
+     *
+     * @return $this
+     */
+    public function setFilter(array $filter): static
+    {
+        $this->filter = $filter;
+        $this->rewind();
+        return $this;
+    }
+
+    /**
+     * Устанавливает поля выборки элементов
+     *
+     * @param array $select Список полей
+     *
+     * @return $this
+     */
+    public function setSelect(array $select): static
+    {
+        $this->select = $select;
+
+        if (!in_array('ID', $this->select)) {
+            $this->select[] = 'ID';
+        }
+
+        return $this;
+    }
+
+    /**
+     * Устанавливает свойства элементов для выборки
+     *
+     * @param array $properties Массив кодов свойств
+     *
+     * @return $this
+     */
+    public function setProperties(array $properties): static
+    {
+        $this->properties = $properties;
+        return $this;
+    }
+
+    /**
+     * Возвращает текущий элемент итератора
+     *
+     * @return mixed
+     */
+    public function current(): mixed
+    {
+        return current($this->batch);
+    }
+
+    /**
+     * Переводит итератор на следующий элемент
+     *
+     * Если текущий батч закончился, подгружает следующий батч.
+     *
+     * @return void
+     */
+    public function next(): void
+    {
+        next($this->batch);
+
+        if (!$this->valid()) {
+            $this->fetchBatch();
         }
     }
 
     /**
-     * Загрузка элементов ИБ
+     * Возвращает ключ текущего элемента итератора
      *
-     * @return Iterator
-     * @author Daniil S.
+     * @return mixed
      */
-    protected function load(): Iterator
+    public function key(): mixed
     {
-        $filter = (array)($this->options['FILTER'] ?? []);
-        $order = (array)($this->options['ORDER'] ?? ['SORT' => 'ASC']);
-        $select = (array)($this->options['SELECT'] ?? []);
+        return key($this->batch);
+    }
 
-        $result = $this->provider
-            ->setFilter($filter)
-            ->setOrder($order)
-            ->setSelect($select)
-            ->query();
+    /**
+     * Проверяет валидность текущего элемента
+     *
+     * @return bool true, если элемент существует, false — если батч закончился
+     */
+    public function valid(): bool
+    {
+        $key = $this->key();
+        return $key !== null && $key !== false;
+    }
 
-        if (is_null($result)) {
-            return new ArrayIterator;
+    /**
+     * Сбрасывает итератор
+     *
+     * Перезапускает итерацию с самого начала
+     *
+     * @return void
+     */
+    public function rewind(): void
+    {
+        $this->lastId = 0;
+        $this->finished = false;
+        $this->batch = [];
+        $this->fetchBatch();
+    }
+
+    /**
+     * Загружает следующий батч элементов из инфоблока
+     *
+     * - Поддерживает фильтрацию по последнему ID
+     * - Загружает поля и свойства
+     * - Обновляет состояние итератора
+     *
+     * @return void
+     */
+    protected function fetchBatch(): void
+    {
+        if ($this->finished) {
+            return;
         }
 
-        $iterator = new ArrayIterator;
+        $filter = $this->buildFilter();
+        $result = $this->executeQuery($filter);
 
-        while ($facade = $result->GetNextElement()) {
-            $item = $facade->GetFields();
+        $this->batch = [];
+
+        if (is_null($result)) {
+            $this->finished = true;
+            return;
+        }
+
+        $this->processResult($result);
+    }
+
+    /**
+     * Строит фильтр для запроса к элементам ИБ
+     *
+     * Добавляет условие по последнему ID для батчевой загрузки.
+     *
+     * @return array
+     */
+    protected function buildFilter(): array
+    {
+        return array_merge(
+            $this->filter,
+            ['>ID' => $this->lastId]
+        );
+    }
+
+    /**
+     * Выполняет запрос к провайдеру элементов ИБ
+     *
+     * @param array $filter
+     *
+     * @return CIBlockResult|null
+     */
+    protected function executeQuery(array $filter): ?CIBlockResult
+    {
+        return $this->provider
+            ->setFilter($filter)
+            ->setOrder(['ID' => 'ASC'])
+            ->setSelect($this->select)
+            ->setLimit($this->limit)
+            ->query();
+    }
+
+    /**
+     * Обрабатывает результат запроса и формирует батч
+     *
+     * @param CIBlockResult $result
+     *
+     * @return void
+     */
+    protected function processResult(CIBlockResult $result): void
+    {
+        while ($element = $result->GetNextElement()) {
+            $item = $element->GetFields();
             $item['PROPERTIES'] = [];
 
-            if (is_iterable($this->options['PROPERTIES'])) {
-                foreach ($this->options['PROPERTIES'] as $code) {
-                    // TODO: Оптимизировать, если будет много свойств, то тогда будет много запросов к базе данных
-                    $property = $facade->GetProperty($code);
-                    if (!empty($property)) {
-                        $item['PROPERTIES'][$code] = $property;
+            $this->lastId = $item['ID'];
+
+            if (!empty($this->properties)) {
+                $properties = $element->GetProperties();
+
+                foreach ($this->properties as $code) {
+                    if (!empty($properties[$code])) {
+                        $item['PROPERTIES'][$code] = $properties[$code];
                     }
                 }
             }
 
-            $iterator->append($item);
+            $this->batch[] = $item;
         }
-
-        return $iterator;
     }
 }
--- tests/Source/Entities/IBlock/ElementTest.php
+++ tests/Source/Entities/IBlock/ElementTest.php
@@ -5,13 +5,9 @@ namespace Sholokhov\Exchange\Source\Entities\IBlock;
 use CIBlockResult;
 use _CIBElement;
 
-use Sholokhov\Exchange\Exception\Source\SourceException;
 use Sholokhov\Exchange\Provider\Entity\IBlockElementProviderInterface;
 
-use Bitrix\Main\Loader;
-
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\Attributes\DataProvider;
 /**
  * Тесты для класса Element.
  *
@@ -22,24 +18,6 @@ use PHPUnit\Framework\Attributes\DataProvider;
 class ElementTest extends TestCase
 {
     /**
-     * Проверяет, что конструктор выбрасывает исключение при неправильном фильтре.
-     *
-     * Использует DataProvider {@see invalidFilterProvider} для передачи различных некорректных опций.
-     *
-     * @param array $options Набор опций, передаваемых в конструктор Element
-     *
-     * @return void
-     *
-     * @throws SourceException Ожидаемое исключение при неверном фильтре
-     */
-    #[DataProvider('invalidFilterProvider')]
-    public function testExceptionFilter(array $options): void
-    {
-        $this->expectException(SourceException::class);
-        new Element($options);
-    }
-
-    /**
      * Проверяет работу метода load() через итератор.
      *
      * Тест создает моки элементов _CIBElement и CIBlockResult, а также мок провайдера IBlockElementProviderInterface.
@@ -49,74 +27,107 @@ class ElementTest extends TestCase
      */
     public function testLoadReturnsIterator(): void
     {
-        Loader::includeModule('iblock');
-
         // Создаем моки элементов _CIBElement
         $element1 = $this->createMock(_CIBElement::class);
         $element1->method('GetFields')->willReturn(['ID' => 1, 'NAME' => 'Alice']);
-        $element1->method('GetProperty')->willReturn([]);
+        $element1->method('GetProperties')->willReturn([]);
 
         $element2 = $this->createMock(_CIBElement::class);
         $element2->method('GetFields')->willReturn(['ID' => 2, 'NAME' => 'Bob']);
-        $element2->method('GetProperty')->willReturn([]);
+        $element2->method('GetProperties')->willReturn([]);
+
+        // Массив элементов для батчей
+        $allElements = [$element1, $element2];
 
         // Мок CIBlockResult
         $resultMock = $this->createMock(CIBlockResult::class);
-        $resultMock->method('GetNextElement')->willReturnOnConsecutiveCalls(
-            $element1,
-            $element2,
-            null // Конец итерации
-        );
+        $resultMock->method('GetNextElement')->willReturnCallback(function () use (&$allElements) {
+            return array_shift($allElements) ?: null;
+        });
 
         // Мок провайдера
         $providerMock = $this->createMock(IBlockElementProviderInterface::class);
         $providerMock->method('setFilter')->willReturnSelf();
         $providerMock->method('setOrder')->willReturnSelf();
         $providerMock->method('setSelect')->willReturnSelf();
+        $providerMock->method('setLimit')->willReturnSelf();
         $providerMock->method('query')->willReturn($resultMock);
 
         // Создаем объект Element с моканным провайдером
-        $sourceOptions = [
-            'FILTER' => ['IBLOCK_ID' => 'test'],
-            'PROPERTIES' => [
-                'PROPERTY1',
-                'PROPERTY3',
-            ]
-        ];
-        $elementSource = new Element($sourceOptions, $providerMock);
+        $source = new Element($providerMock);
 
-        $items = iterator_to_array($elementSource);
+        // Итерация через foreach
+        $items = [];
+        foreach ($source as $item) {
+            $items[] = $item;
+        }
 
         // Проверяем количество элементов
         $this->assertCount(2, $items);
 
-        // Проверяем содержимое первого элемента
+        // Проверяем содержимое
         $this->assertEquals(1, $items[0]['ID']);
         $this->assertEquals('Alice', $items[0]['NAME']);
         $this->assertEquals([], $items[0]['PROPERTIES']);
 
-        // Проверяем содержимое второго элемента
         $this->assertEquals(2, $items[1]['ID']);
         $this->assertEquals('Bob', $items[1]['NAME']);
         $this->assertEquals([], $items[1]['PROPERTIES']);
     }
 
-    /**
-     * Провайдер данных для теста конструктора с некорректным фильтром.
-     *
-     * Возвращает массив наборов опций, которые должны вызывать исключение SourceException.
-     *
-     * @return array<int, array<int, array<string, mixed>>> Наборы некорректных опций
-     */
-    public static function invalidFilterProvider(): array
+    public function testPaginationWorks(): void
     {
-        return [
-            [
-                []
-            ],
-            [
-                ['FILTER' => []]
-            ]
+        // Создадим 5 элементов
+        $elementsData = [
+            ['ID' => 1, 'NAME' => 'Alice'],
+            ['ID' => 2, 'NAME' => 'Bob'],
+            ['ID' => 3, 'NAME' => 'Charlie'],
+            ['ID' => 4, 'NAME' => 'Diana'],
+            ['ID' => 5, 'NAME' => 'Eve'],
         ];
+
+        $elements = [];
+        foreach ($elementsData as $data) {
+            $elementMock = $this->createMock(_CIBElement::class);
+            $elementMock->method('GetFields')->willReturn($data);
+            $elementMock->method('GetProperties')->willReturn([]);
+            $elements[] = $elementMock;
+        }
+
+        // Разобьём элементы на батчи по 2
+        $allElements = $elements;
+
+        // Мок CIBlockResult
+        $resultMock = $this->createMock(CIBlockResult::class);
+        $resultMock->method('GetNextElement')->willReturnCallback(function () use (&$allElements) {
+            return array_shift($allElements) ?: null;
+        });
+
+        // Мок провайдера
+        $providerMock = $this->createMock(IBlockElementProviderInterface::class);
+        $providerMock->method('setFilter')->willReturnSelf();
+        $providerMock->method('setOrder')->willReturnSelf();
+        $providerMock->method('setSelect')->willReturnSelf();
+        $providerMock->method('setLimit')->willReturnSelf();
+        $providerMock->method('query')->willReturn($resultMock);
+
+        // Создаём источник с лимитом 2, чтобы проверить батчи
+        $source = new Element($providerMock);
+        $source->setLimit(2);
+
+        $items = [];
+        foreach ($source as $item) {
+            $items[] = $item;
+        }
+
+        // Проверяем, что получили все элементы
+        $this->assertCount(5, $items);
+
+        // Проверяем данные каждого элемента
+        foreach ($elementsData as $index => $data) {
+            $this->assertEquals($data['ID'], $items[$index]['ID']);
+            $this->assertEquals($data['NAME'], $items[$index]['NAME']);
+            $this->assertEquals([], $items[$index]['PROPERTIES']);
+        }
     }
 }
